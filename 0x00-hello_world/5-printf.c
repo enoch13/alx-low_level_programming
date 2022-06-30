5011046a50cf6d56ac2493ccbd626854750a150f
enoch13 <mwangienoch13@gmail.com>
2022-06-30 17:44:08 +0300

    Task 5 commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - Uses printf to print a string
+ *
+ *Shouldn't return anything (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
