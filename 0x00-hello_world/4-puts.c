5f573578198b4462404e2faafcfa6698dbbb5fb3
enoch13 <mwangienoch13@gmail.com>
2022-06-30 17:36:43 +0300

    Task 4 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Uses puts function to print a line
+ *
+ *Return: Code should return a 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
