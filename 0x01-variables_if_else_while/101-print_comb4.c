f05417c4709fc7b61bcf08e1147d5c33a770ce15
enoch13 <mwangienoch13@gmail.com>
2022-07-01 15:49:54 +0300

    Advanced task 11 commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,10 +20,8 @@ int main(void)
 				putchar((num1 % 10) + '0');
 				putchar((num2 % 10) + '0');
 				putchar((num3 % 10) + '0');
-				
-				if (num1 == 7 && num2 == 8 && num3 == 9){
+		if (num1 == 7 && num2 == 8 && num3 == 9)
 					continue;
-				}
 				putchar(',');
 				putchar(' ');
 			}
