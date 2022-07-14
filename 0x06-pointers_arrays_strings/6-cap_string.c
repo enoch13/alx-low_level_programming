b1e928866b68978fedfd49d89146121e7d4f01d9
enoch13 <mwangienoch13@gmail.com>
2022-07-14 12:20:50 +0300

    Task 6 commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * cap_string - capitalize all words of a string
  * @str: string
@@ -14,6 +15,8 @@ char *cap_string(char *str)
 			i++;
 
 		if (str[i - 1] == ' ' ||
+		    str[i - 1] == '\t' ||
+		    str[i - 1] == '\n' ||
 		    str[i - 1] == ',' ||
 		    str[i - 1] == ';' ||
 		    str[i - 1] == '.' ||
