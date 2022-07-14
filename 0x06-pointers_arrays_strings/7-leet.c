9103ffb0cba346114abf37ffaaf07751910b6777
enoch13 <mwangienoch13@gmail.com>
2022-07-14 10:43:01 +0300

    Task 7 commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,24 +5,20 @@
  *
  * Return: A pointer to the encoded string.
  */
- 
-char *leet(char *s)
+
+char *leet(char *c)
 {
-	int i;
+	char arr[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char replace[] = {'4', '3', '0', '7', '1'};
+	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; c[i] != '\0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; arr[j] != '\0'; j++)
+		{
+			if (c[i] == arr[j])
+				c[i] = replace[j / 2];
+		}
 	}
-
-	return (s);
+	return (c);
 }
