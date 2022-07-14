be23ec0bfc53f2050cc51f6b0356990dc783171b
enoch13 <mwangienoch13@gmail.com>
2022-07-14 11:27:28 +0300

    Task 6 commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,28 +5,26 @@
  *
  * Return: A pointer to the changed string.
  */
- 
-char *cap_string(char *str)
+ char *cap_string(char *str)
 {
-	int i = 0;
-	int j;
-	char mkuu[] = ",;.!?(){}\n\t\" ";
-
-
-	if (str[0] >= 'a' && str[0] <= 'str')
-		str[0] -= 32;
-
-	for (; str[i] != '\0'; i++)
-	{
-		for (j = 0; j < 14; j++)
-		{
-			if (str[i] == mkuu[j])
-			{
-				if (str[i + 1] >= 'a' && str[i + 1] <= 'str')
-					str[i + 1] -= 32;
-			}
-		}
-	}
-
-	return (str);
-}
+	 int i = 0;
+	 int j;
+	 char mkuu[] = ",;.!?(){}\n\t\" ";
+	 
+	 if (str[0] >= 'a' && str[0] <= 'str')
+		 str[0] -= 32;
+	 for (; str[i] != '\0'; i++)
+	 
+	 {
+		 for (j = 0; j < 14; j++)
+		 {
+			 if (str[i] == mkuu[j])
+			 {
+				 if (str[i + 1] >= 'a' && str[i + 1] <= 'str')
+					 str[i + 1] -= 32;
+			 }
+		 }
+	 }
+	 
+	 return (str);
+ }
