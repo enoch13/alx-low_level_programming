ff8a89f7a8405fca815e55d63e0e48cdcb70cc28
enoch13 <mwangienoch13@gmail.com>
2022-07-14 10:52:55 +0300

    Task 6 commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
+ 
+char *cap_string(char *str)
+{
+	int i = 0;
+	int j;
+	char mkuu[] = ",;.!?(){}\n\t\" ";
+
+
+	if (str[0] >= 'a' && str[0] <= 'str')
+		str[0] -= 32;
+
+	for (; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 14; j++)
+		{
+			if (str[i] == mkuu[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'str')
+					str[i + 1] -= 32;
+			}
+		}
+	}
+
+	return (str);
+}
