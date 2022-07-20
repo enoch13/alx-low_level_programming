22e262b8676fbb5469d8d992cb5b25ec694a2e39
enoch13 <mwangienoch13@gmail.com>
2022-07-20 14:07:11 +0300

    Task 4 commit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _pow_recursion - returns the value of x raised to the power of y
- * @x: number
- * @y: the power 
- * Return: the value of x to the power y
+ * _pow_recursion - return the value of x raised to power of y
+ * @x: number value
+ * @y: number power
+ * Return: x to power of y
  */
 
 int _pow_recursion(int x, int y)
