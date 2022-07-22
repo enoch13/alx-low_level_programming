f67ccc94dc5342247ddab95dcc7cb31ace28c399
enoch13 <mwangienoch13@gmail.com>
2022-07-22 11:36:04 +0300

    Task 1 commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the num of args you passed to it
+ * @argc: number of argument count
+ * @argv: argument vector, array of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
