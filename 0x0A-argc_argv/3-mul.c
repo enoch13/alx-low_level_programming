6d3ce7c2a9b663e864ecf3d0f25d3eb77d899918
enoch13 <mwangienoch13@gmail.com>
2022-07-22 23:18:24 +0300

    Task 3 commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - multiplies two numbers
- * @argc: argument counter
- * @argv: argument vector
- * Return: 0 for sucess result, 1 if there is an ERROR
- */
+* main - multiplies two numbers
+* @argc: argument counter
+* @argv: argument vector
+* Return: 0 for sucess result, 1 if there is an ERROR
+*/
 
 int main(int argc, char *argv[])
 {
@@ -16,12 +15,10 @@ if (argc != 3)
 printf("Error\n");
 return (1);
 }
-
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-product = a * b;
-
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	product = a * b;
+	
 	printf("%i\n", product);
-
 	return (0);
 }
