5de66825e48ae11b0cfeeddf5a8e16c837949507
enoch13 <mwangienoch13@gmail.com>
2022-07-22 13:58:13 +0300

    Task 2 commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
 * main - prints all arguments received
 * @argc: number of argument count
@@ -7,10 +8,10 @@
 */
 int main(int argc, char *argv[])
 {
- int i;
- for (i = 0; i < argc; i++)
- {
-  printf("%s\n", argv[i]);
- }
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
  return (0);
 }
