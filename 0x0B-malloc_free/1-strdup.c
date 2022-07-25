6e76a66658f9ec17350f4e8fedd6b588f19e2a8a
enoch13 <mwangienoch13@gmail.com>
2022-07-25 13:20:47 +0300

    Task 1 commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _strdup - Duplicate a string using malloc
+* @str: string to duplicate
+* Return: Pointer to a the new duplicated string
+*/
+char *_strdup(char *str)
+{
+  char *dplc;
+  unsigned int x = 0;
+  unsigned int y = 0;
+  
+  if (str == NULL)
+    return (NULL);
+  
+  while (str[x])
+    x++;
+  
+  dplc = malloc(sizeof(char) * (x + 1));
+  if (dplc == NULL)
+    return (NULL);
+  
+  while (str[y])
+  {
+    dplc[y] = str[y];
+    y++;
+  }
+  dplc[y + 1] = 0;
+  return (dplc);
+}
