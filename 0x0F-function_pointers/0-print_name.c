cb7b986726dcb01cf5f7efe276edd0cbe881abcb
enoch13 <mwangienoch13@gmail.com>
2022-08-03 12:15:05 +0300

    Task 0 commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,10 @@
 #include "function_pointers.h"
 /**
- * print_name - Prints a name from a function pointer
- * @name: char string
- * @f: function pointer for argument
- */
-
+* print_name - Prints a name from a function pointer
+* @name: char string
+* @f: function pointer for argument
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+f(name);
 }
