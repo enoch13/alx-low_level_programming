8efcf3892891b4d48e4220c559e2f270a9845793
enoch13 <mwangienoch13@gmail.com>
2022-08-03 12:33:17 +0300

    Task 0 commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,8 @@
 #include "function_pointers.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
 * print_name - Prints a name from a function pointer
-* @name: char string
-* @f: function pointer for argument
+* @name: The name to be printed
+* @f: function pointer that takes a string argument
 */
 void print_name(char *name, void (*f)(char *))
 {
