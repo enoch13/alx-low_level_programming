3ecb2b22a5c8c809a84c18d25319f4a3eeede48a
enoch13 <mwangienoch13@gmail.com>
2022-08-04 12:46:56 +0300

    Header file commit

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,21 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
+#include <stdio.h>
+/*
+* struct check - paramenters to check against
+* @chr: character type
+* @f: the function to send it to
+*/
+typedef struct check
+{
+char *c;
+void (*f)(va_list str);
+} check_t;
+
+int _putchar(char chr);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
