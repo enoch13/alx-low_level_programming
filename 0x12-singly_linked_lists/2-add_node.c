2ac19f8e2c9f00763f45076a6cf6222117d1d5ea
enoch13 <mwangienoch13@gmail.com>
2022-08-11 14:21:53 +0300

    Task 2 commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,30 +2,30 @@
 #include <string.h>
 #include <stdio.h>
 /**
- * add_node - add a new node at the beginning of `list_t` list
- * @head: double pointer to head
- * @str: string to be saved in new node, must be duplicated
- * Return: Address of new element or NULL if failed
+ * add_node - adds a new node at the beginning of list_t list
+ * @head: address of node
+ * @str: string to add
+ * Return: address of new element, NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	int x;
-	for (x = 0; dup_str[x] != '\0'; x++);
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->len = x;
-	new->next = *head;
-	*head = new;
-	return (*head);
+list_t *new;
+int x;
+for (x = 0; dup_str[x] != '\0'; x++);
+new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+free(new);
+return (NULL);
+}
+new->str = strdup(str);
+if (new->str == NULL)
+{
+free(new);
+return (NULL);
+}
+new->len = x;
+new->next = *head;
+*head = new;
+return (*head);
 }
