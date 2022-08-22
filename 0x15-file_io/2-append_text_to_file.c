10bb9e56402cc4fa09284e56be490d179e443623
enoch13 <mwangienoch13@gmail.com>
2022-08-22 17:02:30 +0300

    Task 2 commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,8 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	for (i = 0; text_content[len]; len++)
-		;
+	for (len = 0; text_content[len]; len++);
 	wr = write(fd, text_content, len);
 	if (wr == -1)
 		return (-1);
