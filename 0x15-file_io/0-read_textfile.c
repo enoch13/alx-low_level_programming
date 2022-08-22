5256ff6a8ac4fea41cf78184a7f02390fd23ebe7
enoch13 <mwangienoch13@gmail.com>
2022-08-22 14:51:47 +0300

    Task 0 commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,36 +7,36 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buff;
+	char *buffer;
 	int fd, rd, wr;
 
 	if (filename == NULL)
 		return (0);
-	buff = malloc(letters);
-	if (buff == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		free(buff);
+		free(buffer);
 		return (0);
 	}
-	rd = read(fd, buff, letters);
+	rd = read(fd, buffer, letters);
 	if (rd == -1)
 	{
-		free(buff);
+		free(buffer);
 		close(fd);
 		return (0);
 	}
-	wr = write(STDOUT_FILENO, buff, rd);
+	wr = write(STDOUT_FILENO, buffer, rd);
 	if (wr == -1)
 	{
-		free(buff);
+		free(buffer);
 		close(fd);
 		return (0);
 	}
 	close(fd);
-	free(buff);
+	free(buffer);
 	return (wr);
 }
