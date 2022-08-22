b31bd4c9acd5bda0863e96efef3f9ace07bb2253
enoch13 <mwangienoch13@gmail.com>
2022-08-22 14:49:14 +0300

    Task 0 commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * read_textfile - Read a text file and print to POSIX stdout
+ * @filename: char string of files name
+ * @letters: number of letters to read and print
+ * Return: number of letters read and printed, or 0 if error
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	int fd, rd, wr;
+
+	if (filename == NULL)
+		return (0);
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+	wr = write(STDOUT_FILENO, buff, rd);
+	if (wr == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	free(buff);
+	return (wr);
+}
